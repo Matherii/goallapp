25cee395984dab064805f00ec34b77a5a577b251
Matherii <112257183+Matherii@users.noreply.github.com>
2023-01-09 00:26:44 +0300

    Update goalapp.py

--- goalapp.py
+++ goalapp.py
@@ -1,9 +1,9 @@
 name = input('Enter your name: ')
 y_old = int(input('What year are we in? '))
 if y_old > 2022:
-    print(f'Happy New year {name}. It is {y_old}')
+    print(f'Happy New year {name}. Congratulations, you made it to {y_old}')
 else:
-    print(f'New year is coming soon {name}.')
+    print(f'New year is coming soon {name}. Sit tight.')
 goals = input("'List down one big goal for the year 2023: ")
 alam = input(f'Should we remind you of {goals}? ')
 if alam == 'yes':
